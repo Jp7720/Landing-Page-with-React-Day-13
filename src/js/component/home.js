f59885bafc353aa45a7498061930c571137957cd
jeffry porras <jeffryporrasbarahona@gmail.com>
2021-03-07 03:52:04 +0000

    finished

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,21 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+//import rigoImage from "../../img/rigo-baby.jpg";
+import { Navbar } from "./navbar";
+import { Jumbotron } from "./jumbotrom";
+import { Card } from "./Card";
+import { Footer } from "./footer";
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container-fluid">
+			<div className="p-3 mb-2 bg-gradient-dark text-dark" />
+			<Navbar />
+			<Jumbotron />
+			<Card />
+
+			<Footer />
 		</div>
 	);
 }
